17f108727a3f23845f1198f6f391efe643c2ad6e
wsdxl <506615839@qq.com>
2017-12-07 09:54:30 +0800

    list-20171207

---
+++ 20171204.py
@@ -0,0 +1,70 @@
+# # python3 列表
+# list=['hello','world',2,6]
+# list2=[1,3,5,6]
+# list3=['how','are','you']
+# list4=['thinks','i','am','fine']
+# list5=[7,8,9,0]
+# print(list[0])
+# print(list2[1:3])
+# print(list3[0])
+
+##更新列表
+
+# list3[0]='who'
+# print(list3[0])
+# print(list)
+
+##删除列表元素 
+
+# del list[2]
+# print(list)
+# print(len(list))
+# print(list.__len__()) 
+# print(list2+list5)
+# print(list3+list4)
+# print(list+list2)
+# print(list+list3)
+# print(list3*2)
+# print(3 in list2)
+# print(2 in list2)
+# for  x in list2:
+#     print(x,end=" ")
+
+# # python列表截取与拼接
+
+# l=['google','taobao','jingdong']
+# print(l[2]) #读取第3个元素
+# print(l[-2]) # 读取倒数第2个元素
+# print(l[1:]) #读取第2个到最后的元素
+
+# print(list2+[6,7,8,9])
+
+# # 嵌套列表
+# a=['a','b','c']
+# n=[1,2,3]
+# x=[a,n]
+# print(x,x[0],x[0][0])
+
+# python 列表函数&方法
+
+list=['hello','world',2,6]
+list2=[1,3,5,6]
+list3=['how','are','you']
+# print(len(list))
+# print(list3.__len__())
+# print(max(list))           #'>' not supported between instances of 'int' and 'str'
+# print(max(list2))
+# print(max(list3))
+# print(min(list))        #'<' not supported between instances of 'int' and 'str'       
+# print(min(list2))
+# print(min(list3))
+
+# python包含以下方法；
+# list.append(list3)  #列表末尾添加新的对象
+# print(list)
+m=['a','b','d','a']
+print(m.count('a'))
+
+
+
+
