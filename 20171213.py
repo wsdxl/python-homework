62b966f750ea1255d0c4ad216470fcd47df6e37d
wsdxl <506615839@qq.com>
2017-12-13 18:34:56 +0800

    列表方法练习-20171213

--- 20171213.py
+++ 20171213.py
@@ -42,4 +42,24 @@ phone_secound = str(random.choice([3, 5, 7, 8]))
 phone = first_num + phone_secound
 for i in range(9):
     phone = phone + str(random.randint(0, 9))
-print(phone)
+print('phone:',phone)
+
+
+'''
+输入三个数，判断是一般三角形，直角三角形，等腰三角形还是等边三角形
+'''
+a = int(input('a:'))
+b = int(input('b:'))
+c = int(input('c:'))
+if a + b > c and a + c > b and b + c > a:
+    if a == b != c or a == c != b or b == c != a:
+        print('a,b,c组成等腰三角形')
+    elif a * a + b * b == c * c or a * a + c * c == b * b or b * b + c * c == a * a:
+        print('a,b,c组成直角三角形')
+    elif a == b == c:
+        print('a,b,c组成等边三角形')
+    else:
+        print('a,b,c组成一般三角形')
+else:
+    print('a、b、c不构成三角形')
+input('点击enter键退出')
