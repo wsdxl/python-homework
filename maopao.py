78233bf467150923d529bf831da5efbbdbda3f4e
wsdxl <506615839@qq.com>
2017-12-15 14:07:28 +0800

    '迭代器解决阿莫斯特朗问题-20171215'

--- maopao.py
+++ maopao.py
@@ -6,3 +6,16 @@ for i in range(0,n):
         if lists[i]>lists[j]:
             lists[i],lists[j]=lists[j],lists[i]
 print(lists)
+
+# lists = [1,20,13,16,5,18]
+# def bubble_sort(lists):
+#     # 冒泡排序
+#     n = len(lists)
+#     for i in range(0, n):
+#         for j in range(i + 1, n):
+#             if lists[i] > lists[j]:
+#                 lists[i], lists[j] = lists[j], lists[i]
+#     return lists
+
+# s=bubble_sort(lists)
+# print(s)
