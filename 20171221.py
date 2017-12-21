b70f0f04c22fac0ba318d77a60c05b6b290ff9f5
wsdxl <506615839@qq.com>
2017-12-21 12:32:02 +0800

    生成器方式将数据写入到csv文件中-20171221

--- 20171221.py
+++ 20171221.py
@@ -1,14 +1,13 @@
+def fobo(n):
+    n1, n2 = 0, 1
+    for i in range(0, n):
+        yield n1
+        n1, n2 = n2, n1 + n2
+
+
 import csv
-def feibonaqi(n):
-    a=b=1
-    for i in range(0,n):
-        yield a 
-        a,b=b,a+b    
-    
-with open('data.csv',mode='a',newline='') as f:
-    w= csv.writer(f)
-    for i in feibonaqi(10000):
+with open('data.csv', mode='a', newline='') as f:
+    w = csv.writer(f)
+    for i in fobo(100):
         w.writerow([str(i)])
     f.close()
-        
-    
