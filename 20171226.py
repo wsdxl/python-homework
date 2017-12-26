28c5b33400c1e1ce30fc1e147f07b9b5aa44b691
wsdxl <506615839@qq.com>
2017-12-26 18:49:26 +0800

    javascript-20171226

---
+++ 20171226.py
@@ -0,0 +1,22 @@
+# class Foo:
+#     def __init__(self,name,age):
+#         self.name=name
+#         self.age=age
+#     def info(self):
+#         print(self.name,self.age)
+
+# x=Foo('jack',18)
+# x.info()
+
+class Animal:
+    def eat(self):
+	    print(self.name,'eat goods')
+    def drink(self):
+        print(self.name,'drink goods')
+    def move(self):
+        print(self.name,'move quick or slow')
+    
+     
+
+
+
